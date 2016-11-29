e21aa9ce6a1c00b83ac3800eaf443960aa598345
Lucas Maystre <lucas@maystre.ch>
2016-11-29 18:45:08 +0100

    Increase the tolerance in EP unit test.
    I relaxed a bit the requirement of "closeness" between the ground truth numpy
    array and the EP estimate, otherwise the tests would fail from time to time.

--- tests/test_ep.py
+++ tests/test_ep.py
@@ -17,5 +17,5 @@ def test_ep_logit():
     with open(data_path("simpletrans-4.dat")) as f:
         num_items, data = parse_pairwise(f.read())
     mean, cov = choix.ep_pairwise(num_items, data, 8.0, model='logit')
-    assert np.allclose(mean, LOGIT_TRUE_MEAN)
-    assert np.allclose(cov, LOGIT_TRUE_COV)
+    assert np.allclose(mean, LOGIT_TRUE_MEAN, atol=1e-3)
+    assert np.allclose(cov, LOGIT_TRUE_COV, atol=1e-3)
