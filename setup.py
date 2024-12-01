42708525e994a3c9706efa7c14b0e437793a7c98
Lucas Maystre <lucas@maystre.ch>
2024-12-01 14:49:58 +0000

    Release version 0.3.6, with fix for issue #25.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
     name='choix',
-    version='0.3.5',
+    version='0.3.6',
     author='Lucas Maystre',
     author_email='lucas@maystre.ch',
     description="Inference algorithms for models based on Luce's choice axiom.",
@@ -36,7 +36,7 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=[
         'pytest',
-        'networkx',
+        'networkx>=1,<3',
     ],
     include_package_data=True,
     zip_safe=False,
